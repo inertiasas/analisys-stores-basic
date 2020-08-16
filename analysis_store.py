4bae603f380f90f79290cb542b06f47d723fcd8e
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2020-08-16 03:15:40 -0500

    verify

--- analysis_store.py
+++ analysis_store.py
@@ -8,21 +8,28 @@
 
 # ## Librerías
 
-# In[1]:
+# In[65]:
 
 
 import requests
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
+from sklearn import model_selection, naive_bayes, svm
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
 
 
 # ## Listado de productos
 
-# In[2]:
+# In[38]:
 
 
-term = 'leche'
+term = 'lacteos'
 url_search = 'https://busqueda.tiendasjumbo.co/busca?q={0}'.format(term)
 search = requests.get(url_search)
 soup_search = BeautifulSoup(search.text, "html5lib")
@@ -34,7 +41,7 @@ print(results[:2])
 # 
 # Extracción de información: Categorias, imagén, nombre, descripción, precio
 
-# In[3]:
+# In[39]:
 
 
 products = []
@@ -67,7 +74,7 @@ print(json.dumps(products[:2], indent=4, sort_keys=True))
 # 
 # ## Extraer campos a analizar
 
-# In[8]:
+# In[40]:
 
 
 df = pd.DataFrame.from_dict(products)
@@ -76,11 +83,99 @@ df = df[col]
 print(df.head())
 
 
-# In[9]:
+# ## Ajustar nombre y verficar datos
+
+# In[41]:
 
 
 df = df[pd.notnull(df['name'])]
 df.columns = ['category', 'name']
 
 
-# # Agregaremos una columna que codificará la categoría como un entero
+# ## Codificar categoría como entero
+
+# In[43]:
+
+
+df['category_id'] = df['category'].factorize()[0]
+category_id_df = df[['category', 'category_id']].drop_duplicates().sort_values('category_id')
+category_to_id = dict(category_id_df.values)
+id_to_category = dict(category_id_df[['category_id', 'category']].values)
+print(category_id_df)
+
+
+# ## Se generan los tokens
+
+# In[44]:
+
+
+df['name'] = [entry.lower() for entry in df['name']]
+print(df.head())
+
+
+# In[48]:
+
+
+df['name']= [word_tokenize(entry) for entry in df['name']]
+print(df.head())
+
+
+# In[52]:
+
+
+for index,entry in enumerate(df['name']):
+    final_words = []
+    stemmer = SnowballStemmer('spanish')
+    for word in entry:
+        if word not in stopwords.words('spanish') and word.isalpha():
+            word_final = stemmer.stem(word)
+            final_words.append(word_final)
+            df.loc[index,'name'] = str(final_words)
+print(df.head())
+
+
+# # Modelo
+# 
+# ## Se dividen los valores en los de entrenamiento y testeo
+
+# In[55]:
+
+
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(df['name'],df['category'],test_size=0.3)
+
+
+# In[58]:
+
+
+Encoder = LabelEncoder()
+Train_Y = Encoder.fit_transform(Train_Y)
+Test_Y = Encoder.fit_transform(Test_Y)
+print(Train_Y)
+
+
+# In[62]:
+
+
+Tfidf_vect = TfidfVectorizer(max_features=5000)
+Tfidf_vect.fit(df['name'])
+print(Tfidf_vect.vocabulary_)
+
+
+# In[63]:
+
+
+Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+print(Train_X_Tfidf)
+
+
+# ## Modelo SVM — Support Vector Machine
+
+# In[66]:
+
+
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+SVM.fit(Train_X_Tfidf,Train_Y)
+predictions_SVM = SVM.predict(Test_X_Tfidf)
+print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, Test_Y)*100)
+
